9b46329fb0f4558ee2383558348bd40c168ed3ac
BlackRece <mauricemlth@hotmail.com>
2022-06-01 20:18:08 +0100

    added buttons to main menu

---
+++ Unity.TriFighter/Assets/TriFighter/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame_Click() {
+        
+    }
+    
+    public void LoadGame_Click(){}
+    public void Options_Click(){}
+    public void QuitGame_Click(){}
+}
