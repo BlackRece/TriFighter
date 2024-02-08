0c05e4026475cd04c90367a6527c4a41dd08c6d0
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2024-02-08 22:32:11 +0000

    adding previous version of TriFighter3D

---
+++ TriFighter3D/Assets/Terrain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour {
+
+    GameObject WallL;
+    GameObject WallR;
+    GameObject Floor;
+
+    public int terrainLength;
+
+    //List<GameObject> terrain;
+
+	void Start () {
+		
+	}
+
+    void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+}
